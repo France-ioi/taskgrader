bf97cb99ef4aaa921a071a2879872f503a5b6c51
Michel Blockelet <blockelet@crans.org>
2015-03-19 20:13:38 +0100

    Licence update + bugfix on grade.py

--- grade.py
+++ grade.py
@@ -1,27 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-### Programme d'évaluation des solutions de France-IOI
-
-# Copyright (c) 2015 France-IOI
+# Copyright (c) 2015 France-IOI, MIT license
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# http://opensource.org/licenses/MIT
 
 
 
@@ -63,7 +45,7 @@ if __name__ == '__main__':
     # Read command line options
     try:
         (opts, files) = getopt.getopt(sys.argv[1:], 'dm:t:p:', ['debug', 'memory-limit=', 'time-limit=', 'task-path='])
-    except getopt.GetOptError as err:
+    except getopt.GetoptError as err:
         print str(err)
         sys.exit(1)
     for (opt, arg) in opts:
