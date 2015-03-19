d822ffbf72f918b199dd9bb04cb0969c1925945f
Michel Blockelet <blockelet@crans.org>
2015-03-19 22:34:27 +0100

    Install.sh update

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 ### Check for dependencies
 ERROR=0
 echo "*** Checking for required binaries..."
-for BINARY in gcc g++ git python2 python3 shar
+for BINARY in fpc gcc g++ gcj git python2 python3 shar sudo
 do
     if which $BINARY > /dev/null
     then
@@ -33,12 +33,12 @@ cd ..
 
 ### Compile C programs
 echo "*** Setting isolate rights..."
-if ! chmod 4755 isolate
+if ! sudo chmod 4755 isolate
 then
     echo "/!\ Failed to \`chmod 4755 isolate\`. Please run the command as root."
     ERROR=1
 fi
-if ! chown root:root isolate
+if ! sudo chown root:root isolate
 then
     echo "/!\ Failed to \`chown root:root isolate\`. Please run the command as root."
     ERROR=1
@@ -48,12 +48,12 @@ echo "*** Compiling box-rights..."
 if gcc -O3 -o box-rights box-rights.c
 then
     echo "Box-rights successfully compiled."
-    if ! chown root:root box-rights
+    if ! sudo chown root:root box-rights
     then
         echo "/!\ Failed to \`chown root:root box-rights\`. Please run the command as root."
         ERROR=1
     fi
-    if ! chmod 4755 box-rights
+    if ! sudo chmod 4755 box-rights
     then
         echo "/!\ Failed to \`chmod 4755 box-rights\`. Please run the command as root."
         ERROR=1
@@ -63,9 +63,14 @@ else
     ERROR=1
 fi
 
-echo "*** Notice :"
-echo "Don't forget to set the variable CFG_BASEDIR in taskgrader.py to an"
-echo "existing directory containing two directories builds/ and cache/."
+echo "*** Notice:"
+echo "Please set, in taskgrader.py:"
+echo "-the variable CFG_BASEDIR in taskgrader.py to an existing clean directory,"
+echo " it will contain the builds and cache;"
+echo "-the variable CFG_BINDIR in taskgrader.py to the directory containing the taskgrader"
+echo " and the binaries compiled by this script;"
+echo "in grade.py:"
+echo "-the variable CFG_TASKGRADER to the path of taskgrader.py."
 
 if [ $ERROR -eq 1 ]
 then
